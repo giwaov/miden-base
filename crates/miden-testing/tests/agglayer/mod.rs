43fbf273a2d8730d3e87d0864abd2f2a738aede5
Marti <marti@miden.team>
2026-02-08 21:39:31 +0000

    chore: re-enable bridge-in tests

--- crates/miden-testing/tests/agglayer/mod.rs
+++ crates/miden-testing/tests/agglayer/mod.rs
@@ -1,9 +1,5 @@
 pub mod asset_conversion;
-// TODO: Uncomment this when https://github.com/0xMiden/miden-base/issues/2397 is ready.
-// The mainnet exit root is hardcoded to pass the current test (i.e. we set the expected mainnet
-// root to whatever the current implementation computes), and changing any impl. details will break
-// the test, forcing us to artificially change the expected root every time.
-// mod bridge_in;
+mod bridge_in;
 mod bridge_out;
 mod crypto_utils;
 mod global_index;
